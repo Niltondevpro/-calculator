8de48fcfbce5bedc5f9cef2f691e4358efef7eec
Niltondevpro <niltoncantanhede11@gmail>
2024-02-19 20:02:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class tela():
+    def __init__(self):
+        self.tela-tk.Tk()
+        self.tela.title("Calculadora")
+        self.tela.resizable(width=False.height=False)
+
+
+
+
+
+        self.tela.mainloop()
+
+
+tela()
